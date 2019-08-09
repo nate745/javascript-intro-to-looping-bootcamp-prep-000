d0f4a569331f07d3e65f3a7d183091f9414aaead
Nathan Alex <nate745@gmail.com>
2019-08-09 09:32:41 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -16,6 +16,13 @@ function whileLoop(n){
   return `done`
 }
 
+var i = 0;
+
+function incrementVariable() {
+  i = i + 1;
+  return i;
+}
+
 function doWhileLoop(num){
   do {
     console.log(`I run once regardless.`);
